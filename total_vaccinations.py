21e2c6aa11f6fcfbb758203bba6a5104a6443f1c
nazliabdussamet <77547828+nazliabdussamet@users.noreply.github.com>
2022-04-04 20:21:47 +0300

    Create total_vaccinations.py

---
+++ total_vaccinations.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import math
+import datetime
+
+df = pd.read_csv("country_vaccination_stats.csv")
+
+countries = df["country"].unique()
+
+min_daily_vaccinations = {}
+
+for i in countries:
+    min_daily_vaccinations[i] = df.loc[df["country"]==i]["daily_vaccinations"].min()
+
+df.loc[df['daily_vaccinations'].isnull(),'value_is_NaN'] = 'Yes'
+df.loc[df['daily_vaccinations'].notnull(), 'value_is_NaN'] = 'No'
+
+for i,row in df.iterrows():
+    if row[4] == "Yes":
+        if math.isnan(min_daily_vaccinations.get(row[0])) == True:
+            df.at[i,'daily_vaccinations'] = 0
+        else:
+            df.at[i,'daily_vaccinations'] = min_daily_vaccinations.get(row[0])
+            
+total_vaccinations = 0
+for i,row in df.iterrows():
+    if row[1] == "1/6/2021":
+        total_vaccinations += row[2]
+
+print(total_vaccinations)
